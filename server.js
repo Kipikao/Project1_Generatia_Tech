412f25927426fef4dc1b1b48d960fa736f2df9b3
Kipikao <150921035+Kipikao@users.noreply.github.com>
2023-11-16 00:44:52 +0200

    update

--- server.js
+++ server.js
@@ -3,26 +3,18 @@ import routerQtn from "./routers/questions.js";
 import morgan from "morgan";
 import mongoose from "mongoose";
 import Qtn from "./model/qtn.js";
-// const Qtn = require("./model/qtn")
+import dotenv from "dotenv";
+dotenv.config();
 
 // express app
 let app = express();
 
 //connect to mongoDB
-const dbURI = "link to mongoDb"
- mongoose
+const dbURI = process.env.MONGOURI;
+mongoose
   .connect(dbURI)
   .then((result) => app.listen(3000))
   .catch((err) => console.log(err));
-
-// const qtn = {
-//   descriptionCategori: "mecanica",
-//   summaryQtn: `xLorem ipsum dolor sit amet, consectetur `,
-//   detailProblem: `xLorem ipsum dolor sit amet, consectetur
-//    adipisicing elit. Ex, esse. Voluptates fuga inventore ullam quo nemo?
-//     Sed labore, totam, voluptatum ea at cumque sapiente nesciunt reiciendis
-//     fugit asperiores quo eligendi.`,
-// };
 
 app.use(express.static("public"));
 app.use(express.urlencoded({ extended: true }));
@@ -45,5 +37,3 @@ app.use("/qtn", routerQtn);
 app.use((req, res) => {
   res.status(404).render("404");
 });
-
-// app.listen(3000);
